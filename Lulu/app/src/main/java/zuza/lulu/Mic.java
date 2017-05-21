a6c966a185d3ee7446acc32878e24b645853c3bd
Zuza <zuzannazborowska@gmail.com>
2017-05-21 23:57:06 +0200

    end of work 21.05.2017  23:56 h

--- Lulu/app/src/main/java/zuza/lulu/Mic.java
+++ Lulu/app/src/main/java/zuza/lulu/Mic.java
@@ -6,13 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Help extends AppCompatActivity {
+public class Mic extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        setContentView(R.layout.activity_mic);
 
+
+        Button button_mic_talk = (Button) findViewById(R.id.button_talk);
+        button_mic_talk.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO : on click mic talking
+
+            }
+        });
 
         Button button_home = (Button) findViewById(R.id.button_home);
         button_home.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +36,7 @@ public class Help extends AppCompatActivity {
             }
         });
 
-    }
 
+    }
 
 }
